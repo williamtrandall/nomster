845e8b28068787661183ceb7f35b009f7426871c
William Randall <williamtrandall@gmail.com>
2019-07-28 17:35:02 +0000

    Adding form for new places and problematic pagination on that form

--- Gemfile
+++ Gemfile
@@ -69,3 +69,8 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
+
+gem 'will_paginate', '~> 3.1.1'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
+gem 'bootstrap-will_paginate'
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,13 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.paginate(:page => params[:page], per_page: 1)
+  end
+
+  def new
+    @place = Place.new
+    @places = Place.paginate(:page => params[:page], per_page: 1)
+    #Need to get rid of the pagination while still defining places
   end
 
 end
